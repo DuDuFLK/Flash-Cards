cfaa522c124be7e15d25dbaafa6fde81f579eb62
DuDuFLK <dias.santos.eduardo@escola.pr.gov.br>
2024-12-06 11:34:51 -0300

    PERGUNTAS SOBRE O FUTBOOL E O TIMÃO

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,66 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Futebool',
+    'Qual a origem do futebol?',
+    'Embora existam diversas teorias sobre as raízes históricas do futebol, acredita-se que o futebol moderno tenha suas origens na Inglaterra, no século XIX. No Brasil, o esporte foi introduzido em 1894 por Charles Miller.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Futebool',
+    'Qual o objetivo principal do jogo de futebol?',
+    'O objetivo principal do futebol é marcar mais gols do que o adversário. Os gols são marcados quando a bola é colocada completamente dentro do gol adversário.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'futebool',
+    'pessoas compõem um time de futebol de campo?',
+    'Um time de futebol de campo é composto por 11 jogadores, incluindo um goleiro.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'futebool',
+    'Melhor brasileiro que joga futebbool?',
+    'Neymar JR'
+)
+criaCartao(
+    'futebool',
+    'Colocação do Corinthias na tabela do brasilerão na rodada atual?',
+    '8 lugar da tabela'
+)
+criaCartao(
+    'futebool',
+    'Atual campeão da champions?',
+    'Real Madrid'
+)criaCartao(o
+    'futebool',
+    'O messi em quantas bola de ouro?',
+    '8 bolas de ouro'
+)criaCartao(
+    'futebool',
+    'nome do maior time do Brasil?',
+    'Corinthias.'
+)
+criaCartao(
+    'futebool',
+    'Qual foi o principal motivo para a construção da Neo Química Arena?',
+    'Modernizar a infraestrutura do clube e sediar a Copa do Mundo de 2014'
+)
+criaCartao(
+    'futebool',
+    'Qual é a principal torcida organizada do Corinthians?',
+    'Gaviões da Fiel'Marcelinho Carioca
+)
+criaCartao(
+    'futebool',
+    'Qual é o significado do símbolo do Corinthians, que representa um "C" estilizado?',
+    'A representação de um escudo'
+)
+criaCartao(
+    'futebool',
+    'Qual foi o apelido carinhoso dado à dupla de ataque formada por Romário e Viola, quando ambos jogaram no Corinthians?',
+    'Os Caçadores'
+)
+criaCartao(
+    'futebool',
+    'Qual jogador, conhecido por sua habilidade com a bola parada, é considerado um dos maiores ídolos da história do Corinthians?',
+    'Marcelinho Carioca'
 )
